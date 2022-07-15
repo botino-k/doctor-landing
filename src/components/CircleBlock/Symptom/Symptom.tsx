0808c27399a759d22a3a98ca0a9b0361ef4cbcc6
botino-k <lassmary@bk.ru>
2022-07-15 20:25:26 +0300

    feat: add circl style text

--- src/components/CircleBlock/Symptom/Symptom.tsx
+++ src/components/CircleBlock/Symptom/Symptom.tsx
@@ -6,11 +6,29 @@ import stl from "./Symptom.module.css";
 
 const Symptom = ({ props }: any) => {
   const symptomBlock = props[0].symptoms.map((item: any, index: number) => {
-    
     const circStyle = {
-      transform: `rotate(calc(360deg/${props[0].symptoms.length}*${index + 1}))`};
+      transform: `rotate(calc(360deg/${props[0].symptoms.length}*${index}))`};
     const rotate = {
-      transform: `rotate(calc(360deg/-${props[0].symptoms.length}*${index + 1}))`};
+      transform: `rotate(calc(360deg/-${props[0].symptoms.length}*${index}))`};
+
+    let descrStyle = {
+      top: ``,
+      left: ``,
+      right: ``,
+      bottom: ``,
+    };
+
+    let deg: number = (360 / props[0].symptoms.length) * index;
+
+    if (deg < 90 || deg > 270) {
+      descrStyle.right = `calc(100% + 30px)`;
+    } else if (deg === 90) {
+      descrStyle.top = `calc(0% - 30px)`;
+    } else if (deg > 90 && deg < 270) {
+      descrStyle.left = `calc(100% + 30px)`;
+    } else if (deg === 270) {
+      descrStyle.top = `calc(150% )`;
+    }
 
     return (
       <div style={circStyle} className={stl.symptomBlock}>
@@ -18,7 +36,9 @@ const Symptom = ({ props }: any) => {
           <div className={stl.icon}>
             <img src={item.img} alt="img" />
           </div>
-          <div className={stl.description}>{item.description}</div>
+          <div style={descrStyle} className={stl.description}>
+            {item.description}
+          </div>
         </div>
       </div>
     );
@@ -31,7 +51,6 @@ const Symptom = ({ props }: any) => {
           <h3 className={stl.header}>{props[0].title} </h3>
           <img className={stl.img} src={props[0].imgCircle} alt="img" />
         </div>
-
         {symptomBlock}
       </div>
     </div>
